f3f6685e830437b26a7d510fc7353a4f01afea10
won choi <hufsie96@naver.com>
2020-08-04 11:12:37 +0900

    error page 추가 및 update 추가

--- src/main/java/com/example/demo/userweb/controller/UserTestController.java
+++ src/main/java/com/example/demo/userweb/controller/UserTestController.java
@@ -3,6 +3,10 @@ package com.example.demo.userweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.userweb.vo.User;
 
@@ -11,8 +15,24 @@ public class UserTestController {
  
     @GetMapping("/userTest")
     public String getUser(Model model) {
-        User user = new User("user01", "멋진언이", "20") ;
+        User user = new User("user01", "홍길동", "20") ;
         model.addAttribute("user", user);
         return "userTest";
     }
+    
+    @GetMapping("/user/{userId}/{name}/{age}")
+    public String getUser(Model model, @PathVariable String userId, @PathVariable String name, @PathVariable String age) {
+        User user = new User(userId, name, age) ;
+        model.addAttribute("user", user);
+        return "userTest";
+    }
+    
+    @PostMapping(path = "/updateUser")
+    public String updateUser(User user, Model model) {
+    	User newUser = new User("New " + user.getUserId(), "New "+user.getName(), user.getAge()) ;
+    	model.addAttribute("user", newUser);
+    	
+    	return "userTest";
+    }
+    
 }
